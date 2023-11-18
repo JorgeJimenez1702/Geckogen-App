0af614871a148194a8e2096ee094794790305375
JorgeJimenez1702 <83251551+JorgeJimenez1702@users.noreply.github.com>
2023-11-18 05:44:58 -0700

    adding TerrariumFunctions
    Creating the terrarium functions folder, and creating the functions of adding and the Interface for a terrarium

---
+++ GeckApp/app/(TerrariumFunctions)/AddTerrarium.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+
+const AddTerrarium = () => {
+  
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>ANDO PROBANDO QUE ME SALGA ALGO EN ESTA PANTALLLA</Text>
+    </View>
+  );
+};
+
+export default AddTerrarium;
--- GeckApp/app/(auth)/terrarium.tsx
+++ GeckApp/app/(auth)/terrarium.tsx
@@ -1,14 +1,54 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
 import React from 'react';
 
 
 const terrarium = () => {
-  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>ANDO PROBANDO QUE ME SALGA ALGO EN ESTA PANTALLLA</Text>
+    <View style={styles.container}>
+      <View style={styles.moduleBox}>
+        <Link href="/(TerrariumFunctions)/TerrariumInterface" asChild>
+          <TouchableOpacity style={styles.moduleTextContainer}>
+            <Text style={styles.moduleText}>Module 1</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 30,
+    backgroundColor: '#F8F8F8',
+  },
+  moduleBox: {
+    width: 341,
+    height: 193,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#E2E2E2',
+    marginVertical: 20,
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 1
+  },
+  moduleTextContainer: {
+    marginTop: 142,
+    marginLeft: 25,
+  },
+  moduleText:{
+    color: '#000',
+    fontSize: 17,
+    fontStyle: 'normal',
+    fontWeight: '700',
+  },
+});
 
 export default terrarium;
