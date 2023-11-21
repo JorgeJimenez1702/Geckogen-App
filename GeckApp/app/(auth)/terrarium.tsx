99d8a80b306011071a5f5cef4a99538aed04742b
JorgeJimenez1702 <83251551+JorgeJimenez1702@users.noreply.github.com>
2023-11-20 18:57:17 -0700

    terrarium fetch implementation

--- GeckApp/app/(auth)/terrarium.tsx
+++ GeckApp/app/(auth)/terrarium.tsx
@@ -1,19 +1,60 @@
-import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { Link } from 'expo-router';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Spinner from 'react-native-loading-spinner-overlay';
 
+interface itemInterface {
+  id: string,
+  name: string,
+  humidity: string,
+  light_Cycle: string,
+  oxigenation: string,
+  temperature: string
+}
 
 const terrarium = () => {
+  const [terrariums, setTerrariums] = useState<itemInterface[]>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getTerrariums = async () => {
+      try {
+        const response = await fetch('https://3421-187-137-2-145.ngrok-free.app/geckogen-a0538/us-central1/app/api/terrariums');
+        if (response.ok) {
+          const result = await response.json();
+          setTerrariums(result);
+          setIsLoading(false);
+        } else {
+          console.error('Failed to fetch terrariums');
+        }
+      } catch (error) {
+        console.error('Error fetching terrariums:', error);
+      }
+    };
+    getTerrariums();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <View style={styles.moduleBox}>
-        <Link href="/(TerrariumFunctions)/TerrariumInterface" asChild>
-          <TouchableOpacity style={styles.moduleTextContainer}>
-            <Text style={styles.moduleText}>Module 1</Text>
-          </TouchableOpacity>
-        </Link>
-      </View>
+
+      {terrariums && !isLoading ? (
+
+        terrariums.map((terrariums, idx) => {
+          return (
+          <View style={styles.moduleBox} key={idx}>
+            <Link href="/(TerrariumFunctions)/TerrariumInterface" asChild>
+              <TouchableOpacity style={styles.moduleTextContainer}>
+                <Text style={styles.moduleText}>{terrariums.name}</Text>
+              </TouchableOpacity>
+            </Link>
+          </View>
+          );
+        })
+
+      ) : (
+        <Spinner visible={isLoading} />
+      )}
+
     </View>
   );
 };
@@ -43,7 +84,7 @@ const styles = StyleSheet.create({
     marginTop: 142,
     marginLeft: 25,
   },
-  moduleText:{
+  moduleText: {
     color: '#000',
     fontSize: 17,
     fontStyle: 'normal',
